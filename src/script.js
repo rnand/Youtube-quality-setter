fc2f90febe0c3c440b23778a8fb8469e00d51df5
Rahul Anand <rahul1989@gmail.com>
2015-09-30 22:33:46 +0530

    Trial

--- src/script.js
+++ src/script.js
@@ -1,6 +1,6 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.executeScript({
-    'document.body.style.backgroundColor="red"'
+    'document.body.style.backgroundColor="blue";document.body.backgroundColor="red";'
     //do some stuff here
   });
 });
